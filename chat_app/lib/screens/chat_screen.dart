8deac03e7a5468a8651c5ff698a597060d00f6eb
Binoy T V <binoytv9@gmail.com>
2020-07-04 11:58:13 +0530

    realtime message

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -9,16 +10,47 @@ class ChatScreen extends StatelessWidget {
           'Chat',
         ),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(
-            8,
-          ),
-          child: Text(
-            'This works!',
-          ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/4mVu9gdIDDtCjS0QyVvG/messages')
+            .snapshots(),
+        builder: (
+          ctx,
+          streamSnapshot,
+        ) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(
+                8,
+              ),
+              child: Text(
+                documents[index]['text'],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
         ),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/4mVu9gdIDDtCjS0QyVvG/messages')
+              .add(
+            {
+              'text': 'This was added by clicking by button!',
+            },
+          );
+        },
       ),
     );
   }
