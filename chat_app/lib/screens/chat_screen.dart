1a0475c37ba5172104fb8fa09d668ff79be21afa
Binoy T V <binoytv9@gmail.com>
2020-07-12 13:02:17 +0530

    push notification

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,10 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/new_message.dart';
 import '../widgets/chat/messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg) {
+        print('onMessage $msg');
+        return;
+      },
+      onLaunch: (msg) {
+        print('onLaunch $msg');
+        return;
+      },
+      onResume: (msg) {
+        print('onResume $msg');
+        return;
+      },
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
